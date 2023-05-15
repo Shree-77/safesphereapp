dbd600e1f084b0922499ecc261ffa236ce007f76
Shree-77 <shreedhoni7@gmail.com>
2023-05-15 11:47:49 +0530

    Implementedimagepicker Function

--- lib/Pages/register_page.dart
+++ lib/Pages/register_page.dart
@@ -13,6 +13,7 @@ import '../Services/database_services.dart';
 
 import '../widgets/custom_input_field.dart';
 import '../widgets/rounded_button.dart';
+import '../widgets/rounded_image.dart';
 
 //Providers
 import '../Providers/authentication_provider.dart';
@@ -25,12 +26,63 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+
+  PlatformFile? _profileImage;
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        padding: EdgeInsets.symmetric(
+            horizontal: _deviceWidth * 0.03, vertical: _deviceHeight * 0.02),
+        height: _deviceHeight * 0.98,
+        width: _deviceWidth * 0.97,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _profileImageField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _profileImageField() {
+    return GestureDetector(
+      onTap: () {
+        GetIt.instance.get<MediaService>().pickImageFromLibrary().then((_file) {
+          setState(
+            () {
+              _profileImage = _file;
+            },
+          );
+        });
+      },
+      child: () {
+        if (_profileImage != null) {
+          return RoundedImageFile(
+            key: UniqueKey(),
+            image: _profileImage!,
+            size: _deviceHeight * 0.15,
+          );
+        } else {
+          return RoundedImageNetwork(
+              key: UniqueKey(),
+              imagePath: "https://i.pravatar.cc/150?img=12",
+              size: _deviceHeight * 0.15);
+        }
+      }(),
+    );
   }
 }
